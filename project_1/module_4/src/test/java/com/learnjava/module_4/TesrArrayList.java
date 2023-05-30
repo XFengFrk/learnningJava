72d85b31513c8a74d972f6c00b987d1cf88c5d5d
XFengFrk <534655691@qq.com>
2023-05-30 14:36:13 +0800

    learn list_1

--- project_1/module_4/src/test/java/com/learnjava/module_4/TesrArrayList.java
+++ project_1/module_4/src/test/java/com/learnjava/module_4/TesrArrayList.java
@@ -1,4 +1,18 @@
 package com.learnjava.module_4;
 
+import java.util.ArrayList;
+
 public class TesrArrayList {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList(5);//初始容量5，默认初始容量10
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
+        //内部封装了扩容逻辑，按1.5倍扩容
+        list.add("f");
+
+        System.out.println(list);
+    }
 }
